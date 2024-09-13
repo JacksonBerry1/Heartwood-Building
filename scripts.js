caa74ff279e771d03b1edf6329ba50534ea09603
JacksonBerry1 <jacksongcberry@gmail.com>
2024-09-12 18:33:44 -0700

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,26 @@
+const carousel = document.querySelector('.carousel');
+const carouselInner = document.querySelector('.carousel-inner');
+const carouselItems = document.querySelectorAll('.carousel-item');
+const carouselPrev = document.querySelector('.carousel-prev');
+const carouselNext = document.querySelector('.carousel-next');
+
+let currentSlide = 0;
+
+carouselPrev.addEventListener('click', () => {
+  currentSlide--;
+  if (currentSlide < 0) currentSlide = carouselItems.length - 1;
+  updateCarousel();
+});
+
+carouselNext.addEventListener('click', () => {
+  currentSlide++;
+  if (currentSlide >= carouselItems.length) currentSlide = 0;
+  updateCarousel();
+});
+
+function updateCarousel() {
+  carouselItems.forEach((item, index) => {
+    item.classList.remove('active');
+    if (index === currentSlide) item.classList.add('active');
+  });
+}
